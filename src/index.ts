f4882180331485a5ceccae9ec3418880f20d7256
Joseph <jsphkhan@gmail.com>
2023-03-02 18:30:04 +0400

    updated index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import isClient from "./utils/ssr/isClient";
 import isServer from "./utils/ssr/isServer";
+import findQitafPoints from "./utils/qitaf/findQitafPoints";
+import getFormData from "./utils/document/getFormData";
+import getAddOnNotFound from "./utils/error/getAddOnNotFound";
 
 export {
     isClient,
-    isServer
+    isServer,
+    findQitafPoints,
+    getFormData,
+    getAddOnNotFound
 }
